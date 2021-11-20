31155a05b97e89271bee1a4594978925b95c3f2f
Navar Kartalian <alpha.hunter88@gmail.com>
2021-11-20 09:39:00 -0300

    Route animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
-import { AppProps } from 'next/app'
-import { ChakraProvider } from "@chakra-ui/react"
+import { AppProps } from 'next/app';
+import { ChakraProvider, Box, BoxProps } from "@chakra-ui/react";
+import { motion } from 'framer-motion';
 import { theme } from '../styles/theme';
 import { Header } from '../components/Header';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  const MotionBox = motion<BoxProps>(Box);
+
   return (
     <ChakraProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
+      <MotionBox
+        key={router.route}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: '0.5' }}
+      >
+        <Header />
+        <Component {...pageProps} />
+      </MotionBox>
     </ChakraProvider>
   );
 }
